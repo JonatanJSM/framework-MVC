034f6b146675678b2074aebd8bbce333afe0c6fc
Jonatan <jsantanamay@gmail.com>
2022-11-09 00:17:23 -0600

    Subiendo archivos principales

---
+++ src/test/NewMain.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ *
+ * @author jonat
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ya no puedo más Martha :'C");
+    }
+    
+}
