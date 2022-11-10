20975c374564a16cce523ec49eebf980455e8954
Jonatan <jsantanamay@gmail.com>
2022-11-10 02:20:05 -0600

    Se eliminaron carpetas y archivos
    Se acomodaron mejor los archivos, se eliminaron archivos repetidos. Se quitaron algunos ejemplos de la lectura del JSON. En un commit anterior están

--- src/lecturaJSON/pruebas.java
+++ src/lecturaJSON/pruebas.java
@@ -5,14 +5,9 @@ public class pruebas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       // lector lector = new lector();
-       // lector.obtenerContenidoJSON();
-       // lector.imprimir();
         lectorJSON lec = new lectorJSON();
         lec.obtenerConfiguracionesMVC("confis");
         lec.imprimirConfiguraciones();
         lec.getTransacciones();
-//        construccionJSON aa = new construccionJSON();
-//        aa.obtenerContenidoJSON();
     }
 }
--- src/vistas/VistaLogIn.java
+++
@@ -1,9 +0,0 @@
-package vistas;
-
-import javax.swing.*;
-
-public class VistaLogIn {
-    private JPanel mainPanel;
-    private JButton enviarButton;
-    private JTextField textField1;
-}
