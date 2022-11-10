3016fe2057a7403d6dda187f6943b3b5e2821df1
Jonatan <jsantanamay@gmail.com>
2022-11-09 23:33:05 -0600

    Desarrollo lectura

---
+++ src/configuracion/pruebas.java
@@ -0,0 +1,16 @@
+package configuracion;
+
+public class pruebas {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        lector lector = new lector();
+        lector.obtenerContenidoJSON();
+       // lector.imprimir();
+
+
+//        construccionJSON aa = new construccionJSON();
+//        aa.obtenerContenidoJSON();
+    }
+}
