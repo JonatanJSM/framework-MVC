ad67078736c7c9fb7b4cef637bed5f26552e11ef
Natali <a17001060@gmail.com>
2022-11-10 23:56:55 -0600

    Excepción de configuraciones
    Implementación de una excepción que ayude a verificar que todas las configuraciones se encuentren en el documento.

--- src/lecturaJSON/pruebas.java
+++ src/lecturaJSON/pruebas.java
@@ -7,12 +7,12 @@ public class pruebas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       // lectorJSON lec = new lectorJSON();
+        lectorJSON lec = new lectorJSON();
        // lec.obtenerConfiguracionesMVC("confis");
        // lec.imprimirConfiguraciones();
        // lec.getTransacciones();
-        Transaccion tran = new Transaccion("ejemplo.modelo.modeloVistaEjemplo1","ejemplo.controlador.demoControlador");
-        tran.execute("ejemplo.modelo.modeloVistaEjemplo1","hola");
+        //Transaccion tran = new Transaccion("ejemplo.modelo.modeloVistaEjemplo1","ejemplo.controlador.demoControlador");
+        //tran.execute("ejemplo.modelo.modeloVistaEjemplo1","hola");
         //tran.crearModelo("ejemplo.modelo.modeloVistaEjemplo1");
         //tran.crearControlador("ejemplo.controlador.demoControlador");
     }
